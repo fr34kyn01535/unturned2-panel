2d91ce9e28de88e366af0afcc94cb157d972a1aa
Sven Mawby <fr34kyn01535@bam.yt>
2019-12-30 16:16:29 +0100

    .

--- unturned2-panel/index.js
+++ unturned2-panel/index.js
@@ -5,10 +5,13 @@ const WebServer_1 = require("./src/WebServer");
 const WebSocketServer_1 = require("./src/WebSocketServer");
 exports.WebSocketServer = WebSocketServer_1.default;
 const PluginManager_1 = require("./src/PluginManager");
-const www = new WebServer_1.default(ConfigManager_1.default);
-const ws = new WebSocketServer_1.default(www.server, ConfigManager_1.default);
-const pluginManager = new PluginManager_1.default(ws, www);
-pluginManager.loadPlugins();
+var args = process.argv.slice(2);
+if (args[0] == "run") {
+    const www = new WebServer_1.default(ConfigManager_1.default);
+    const ws = new WebSocketServer_1.default(www.server, ConfigManager_1.default);
+    const pluginManager = new PluginManager_1.default(ws, www);
+    pluginManager.loadPlugins();
+}
 var WebServer_2 = require("./src/WebServer");
 exports.View = WebServer_2.View;
 //# sourceMappingURL=index.js.map
--- unturned2-panel/index.ts
+++ unturned2-panel/index.ts
@@ -3,13 +3,14 @@ import WebServer from "./src/WebServer";
 import WebSocketServer from "./src/WebSocketServer";
 import PluginManager from "./src/PluginManager";
 
-const www = new WebServer(configuration);
+var args = process.argv.slice(2);
 
-const ws = new WebSocketServer(www.server, configuration);
-
-const pluginManager = new PluginManager(ws,www);
-
-pluginManager.loadPlugins();
+if(args[0] == "run"){
+    const www = new WebServer(configuration);
+    const ws = new WebSocketServer(www.server, configuration);
+    const pluginManager = new PluginManager(ws,www);
+    pluginManager.loadPlugins();
+}
 
 
 export { WebSocketServer }
